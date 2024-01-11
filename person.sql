ffa508bccb1437e55d152da62b5f7e668772af83
anggasmkn1gombong <122070352+anggasmkn1gombong@users.noreply.github.com>
2024-01-11 21:29:26 +0700

    Add files via upload

--- person.sql
+++ person.sql
@@ -1,25 +1,32 @@
-insert into person (id, first_name, last_name, email, ip_address) values (1, 'Shoshanna', 'Christaeas', 'schristaeas0@pagesperso-orange.fr', '185.212.144.33');
-insert into person (id, first_name, last_name, email, ip_address) values (2, 'Raul', 'Jewkes', 'rjewkes1@people.com.cn', '51.157.175.210');
-insert into person (id, first_name, last_name, email, ip_address) values (3, 'Anitra', 'Tythe', 'atythe2@narod.ru', '67.64.144.0');
-insert into person (id, first_name, last_name, email, ip_address) values (4, 'Fionna', 'Lurriman', 'flurriman3@people.com.cn', '38.146.117.248');
-insert into person (id, first_name, last_name, email, ip_address) values (5, 'Washington', 'Labbati', 'wlabbati4@sitemeter.com', '255.159.19.22');
-insert into person (id, first_name, last_name, email, ip_address) values (6, 'Jacquie', 'Ferrant', 'jferrant5@nymag.com', '249.223.3.245');
-insert into person (id, first_name, last_name, email, ip_address) values (7, 'Juana', 'Wykes', 'jwykes6@bigcartel.com', '36.138.171.198');
-insert into person (id, first_name, last_name, email, ip_address) values (8, 'Rozelle', 'Barajas', 'rbarajas7@rakuten.co.jp', '16.53.26.68');
-insert into person (id, first_name, last_name, email, ip_address) values (9, 'Simeon', 'Gamet', 'sgamet8@wikispaces.com', '90.123.149.212');
-insert into person (id, first_name, last_name, email, ip_address) values (10, 'Chadd', 'Lovett', 'clovett9@wikispaces.com', '20.155.163.20');
-insert into person (id, first_name, last_name, email, ip_address) values (11, 'Rolfe', 'Ewenson', 'rewensona@latimes.com', '181.253.8.119');
-insert into person (id, first_name, last_name, email, ip_address) values (12, 'Charmian', 'Spinelli', 'cspinellib@google.co.uk', '8.92.47.27');
-insert into person (id, first_name, last_name, email, ip_address) values (13, 'Stephanie', 'Shalders', 'sshaldersc@mozilla.com', '215.111.120.103');
-insert into person (id, first_name, last_name, email, ip_address) values (14, 'Hagan', 'Blythe', 'hblythed@drupal.org', '136.13.158.199');
-insert into person (id, first_name, last_name, email, ip_address) values (15, 'Valeda', 'Middlewick', 'vmiddlewicke@google.it', '20.235.113.22');
-insert into person (id, first_name, last_name, email, ip_address) values (16, 'Cordell', 'Drew-Clifton', 'cdrewcliftonf@netvibes.com', '38.196.216.198');
-insert into person (id, first_name, last_name, email, ip_address) values (17, 'Ileane', 'Ganford', 'iganfordg@home.pl', '211.197.151.6');
-insert into person (id, first_name, last_name, email, ip_address) values (18, 'Augusto', 'Vernalls', 'avernallsh@wiley.com', '147.70.160.226');
-insert into person (id, first_name, last_name, email, ip_address) values (19, 'Booth', 'Aitcheson', 'baitchesoni@live.com', '149.82.66.59');
-insert into person (id, first_name, last_name, email, ip_address) values (20, 'Marcelline', 'Birtwistle', 'mbirtwistlej@deviantart.com', '114.226.23.207');
-insert into person (id, first_name, last_name, email, ip_address) values (21, 'Deirdre', 'Dooney', 'ddooneyk@tripod.com', '155.115.157.127');
-insert into person (id, first_name, last_name, email, ip_address) values (22, 'Verne', 'Sitlington', 'vsitlingtonl@instagram.com', '148.189.103.196');
-insert into person (id, first_name, last_name, email, ip_address) values (23, 'Leia', 'Kirwan', 'lkirwanm@state.gov', '70.234.131.65');
-insert into person (id, first_name, last_name, email, ip_address) values (24, 'Ambrosi', 'Byles', 'abylesn@cbslocal.com', '28.39.126.49');
-insert into person (id, first_name, last_name, email, ip_address) values (25, 'Marjie', 'Hinchon', 'mhinchono@amazon.co.uk', '206.247.192.67');
+create table person (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	ip_address VARCHAR(20)
+);
+insert into person (id, first_name, last_name, email, ip_address) values (1, 'Arline', 'Dolligon', 'adolligon0@ezinearticles.com', '52.128.222.41');
+insert into person (id, first_name, last_name, email, ip_address) values (2, 'Zach', 'McUre', 'zmcure1@hud.gov', '74.88.13.108');
+insert into person (id, first_name, last_name, email, ip_address) values (3, 'Vassili', 'Mott', 'vmott2@amazon.co.jp', '208.189.175.6');
+insert into person (id, first_name, last_name, email, ip_address) values (4, 'Beth', 'Droogan', 'bdroogan3@businesswire.com', '12.177.173.188');
+insert into person (id, first_name, last_name, email, ip_address) values (5, 'Claiborn', 'Feares', 'cfeares4@amazonaws.com', '19.78.92.238');
+insert into person (id, first_name, last_name, email, ip_address) values (6, 'Mercy', 'Baldung', 'mbaldung5@cpanel.net', '160.245.175.192');
+insert into person (id, first_name, last_name, email, ip_address) values (7, 'Gipsy', 'Whiscard', 'gwhiscard6@wiley.com', '139.207.173.40');
+insert into person (id, first_name, last_name, email, ip_address) values (8, 'Silvanus', 'Bricklebank', 'sbricklebank7@odnoklassniki.ru', '237.104.91.217');
+insert into person (id, first_name, last_name, email, ip_address) values (9, 'Winni', 'Stanbrooke', 'wstanbrooke8@telegraph.co.uk', '236.81.170.229');
+insert into person (id, first_name, last_name, email, ip_address) values (10, 'Amelie', 'Guilloux', 'aguilloux9@ebay.com', '145.70.78.148');
+insert into person (id, first_name, last_name, email, ip_address) values (11, 'Thomasa', 'Ashborn', 'tashborna@infoseek.co.jp', '178.9.222.127');
+insert into person (id, first_name, last_name, email, ip_address) values (12, 'Jimmie', 'Skipsea', 'jskipseab@vinaora.com', '195.138.89.64');
+insert into person (id, first_name, last_name, email, ip_address) values (13, 'Lion', 'Jumonet', 'ljumonetc@youtu.be', '79.41.229.54');
+insert into person (id, first_name, last_name, email, ip_address) values (14, 'Grace', 'Jerrim', 'gjerrimd@desdev.cn', '108.189.30.167');
+insert into person (id, first_name, last_name, email, ip_address) values (15, 'Gustie', 'Keasley', 'gkeasleye@cyberchimps.com', '215.65.32.120');
+insert into person (id, first_name, last_name, email, ip_address) values (16, 'Burnard', 'Keri', 'bkerif@gizmodo.com', '98.89.111.141');
+insert into person (id, first_name, last_name, email, ip_address) values (17, 'Lyndel', 'Keel', 'lkeelg@wikia.com', '154.26.216.120');
+insert into person (id, first_name, last_name, email, ip_address) values (18, 'Farand', 'Orsman', 'forsmanh@ovh.net', '186.22.113.207');
+insert into person (id, first_name, last_name, email, ip_address) values (19, 'Yvonne', 'Rodenburg', 'yrodenburgi@multiply.com', '176.103.137.38');
+insert into person (id, first_name, last_name, email, ip_address) values (20, 'Hadleigh', 'Kellitt', 'hkellittj@mtv.com', '233.254.234.182');
+insert into person (id, first_name, last_name, email, ip_address) values (21, 'Thorpe', 'Nanninini', 'tnannininik@bing.com', '33.202.143.216');
+insert into person (id, first_name, last_name, email, ip_address) values (22, 'Ardys', 'Francois', 'afrancoisl@phoca.cz', '70.152.75.65');
+insert into person (id, first_name, last_name, email, ip_address) values (23, 'Guss', 'Gilks', 'ggilksm@amazon.co.jp', '110.113.167.122');
+insert into person (id, first_name, last_name, email, ip_address) values (24, 'Godiva', 'Coole', 'gcoolen@freewebs.com', '163.150.107.112');
+insert into person (id, first_name, last_name, email, ip_address) values (25, 'Eran', 'Blinkhorn', 'eblinkhorno@i2i.jp', '29.163.24.157');
